0b3a8ef9d08c4e1b959b938acaafda4c22fca1e6
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-12-13 22:13:00 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,15 +5,15 @@ variable region {
 
 variable ami_id {
     type = string
-    default = "ami-06d4b7182ac3480fa"
+    default = ""
 }
 
 variable instance_type {
     type = string
-    default = "t2.micro"
+    default = ""
 }
 
 variable az {
     type = string
-    default = "us-east-2a"
+    default = ""
 }
