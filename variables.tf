44c6509ec6a5206e6c2c579e5852231d65b494ae
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-12-13 22:08:53 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable region {
+    type = string
+    default = "us-east-2"
+}
+
+variable ami_id {
+    type = string
+    default = "ami-06d4b7182ac3480fa"
+}
+
+variable instance_type {
+    type = string
+    default = "t2.micro"
+}
+
+variable az {
+    type = string
+    default = "us-east-2a"
+}
